a8eb3b81e5b17fc709faa4a39300675ef2f91b87
Sandra Burri <wohlfuehlquelle@gmail.com>
2018-04-28 13:31:43 +0200

    Layout abgeändert

--- _header.php
+++ _header.php
@@ -20,7 +20,7 @@
                 <img src="img/logo.png" id="logo" />
             </div>
             <div class="col-md-6 text-right">
-                <?php if(isset($_SESSION['user'])){ ?>
+                <?php if(isset($_SESSION['user'])) { ?>
                 User: <?php echo $_SESSION['user']; ?>
                 <a href="logout.php">Logout</a>
                 <?php } ?>
@@ -30,3 +30,54 @@
 
     <div class="container-fluid">
 
+<?php if(isset($_SESSION['user'])) { ?>
+<?php
+    
+    $patientID = 0;
+    if (isset($_GET['id'])){
+      $patientID = (int)($_GET['id']);
+    }
+
+    $functionID = $_SESSION["functionID"];
+    $isNurse = $functionID == 1;
+    $isDoctor = $functionID == 2;
+    $isSecretary = $functionID == 3;    
+
+    function renderLink($url, $title) {
+        $currentPage = $_SERVER["REQUEST_URI"];
+        $active = strpos($currentPage, $url) > 0 ? ' active' : '';
+
+        echo '<li class="nav-item'.$active.'">';
+        echo '<a class="nav-link" href="'.$url.'">'.$title.'</a>';
+        echo '</li>';
+    }
+
+?>
+
+<ul class="my-nav">
+    
+    <?php if ($isSecretary) { ?>   
+        <?php renderLink("newPatients.php", "Neuer Patient erfassen") ?>
+        <?php renderLink("newStaff.php", "Neuer Mitarbeiter erfassen") ?>
+        <?php renderLink("newMedicaments.php", "Neues Medikament erfassen") ?>
+    <?php } ?>
+
+    <?php if ($isDoctor || $isNurse) { ?>
+
+        <?php renderLink("listPatients.php", "Patienten") ?>
+
+        <?php if ($patientID) { ?>
+            <?php renderLink("stammdaten.php?id=$patientID", "Stammdaten") ?>
+            <?php renderLink("vitalsign.php?id=$patientID", "Vitalzeichen") ?>
+            <?php renderLink("medicament.php?id=$patientID", "Medikamente") ?>
+
+            <?php if ($functionID == 2) { /* doctor */ ?>
+                <?php renderLink("medicamentPrescription.php?id=$patientID", "Medikament verschreiben") ?>
+            <?php } ?>        
+        <?php } ?>
+
+    <?php } ?>
+
+</ul>
+
+<?php } ?>
